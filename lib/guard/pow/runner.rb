e608fe0bdffdf13cfb5884091b567d346911b810
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2011-10-20 12:44:27 +0300

    Added custom restart.

--- lib/guard/pow/runner.rb
+++ lib/guard/pow/runner.rb
@@ -5,8 +5,12 @@ module Guard
     class Runner
 
       def restart_pow
-        FileUtils.mkdir 'tmp' unless File.directory?("tmp")
-        FileUtils.touch 'tmp/restart.txt'
+        FileUtils.mkdir 'tmp' unless File.directory?("tmp")        
+        if File.exists?("script/restart_pow")
+          system("script/restart_pow")
+        else
+          FileUtils.touch 'tmp/restart.txt'
+        end
         UI.info "Pow restarted."
         Notifier.notify("Pow restarted.", :title => "Pow", :image => :success)
       end
